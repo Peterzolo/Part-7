dc6c6ec2c58a090761db38dd175d74778455167c
Peter <petersolomon704@gmail.com>
2023-05-03 10:03:46 +0100

    Update AnecdoteList.jsx
    Modified anecdote list

--- src/components/anecdotes/AnecdoteList.jsx
+++ src/components/anecdotes/AnecdoteList.jsx
@@ -1,9 +1,19 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 import "../../components/anecdotes/Anecdote.css";
+import { getAllAnecdotes } from "../../services/anecdoteService";
 
 const AnecdoteList = () => {
   const [anecdotes, setAnecdotes] = useState([]);
+  console.log("ANECDOTES ALL", anecdotes);
+
+  useEffect(() => {
+    const fetchAllAnecdotes = async () => {
+      const response = await getAllAnecdotes();
+      setAnecdotes(response);
+    };
+    fetchAllAnecdotes();
+  }, []);
   return (
     <div className="anecdote-list-wrap">
       <h3 className="title">Anecdotes</h3>
